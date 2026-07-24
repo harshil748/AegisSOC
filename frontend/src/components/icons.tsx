1418acc1cd78e5d074531ae9b9ee091d1bd55889
harshil748 <pharshil748@gmail.com>
2026-07-24 20:13:17 +0530

    ui: update icon components with new icon definitions

--- frontend/src/components/icons.tsx
+++ frontend/src/components/icons.tsx
@@ -74,20 +74,21 @@ export function AuditIcon(props: IconProps) {
     </>,
   );
 }
-export function ReplayIcon(props: IconProps) {
-  return base(
-    props,
-    <>
-      <path d="M3 12a9 9 0 1 0 3-6.7" />
-      <path d="M3 4v5h5" />
-    </>,
-  );
-}
 export function MetricsIcon(props: IconProps) {
   return base(
     props,
     <>
       <path d="M4 20V10M11 20V4M18 20v-7" />
+    </>,
+  );
+}
+export function DemoIcon(props: IconProps) {
+  return base(
+    props,
+    <>
+      <path d="M3 12a9 9 0 1 0 3-6.7" />
+      <path d="M3 4v5h5" />
+      <path d="m10 9 5 3-5 3V9Z" />
     </>,
   );
 }
